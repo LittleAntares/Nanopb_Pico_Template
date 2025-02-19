c1ae86dcb28ebf18a58dfce6b6e010afcb0e1492
LittleAntares <kseangchy@gmail.com>
2025-02-19 22:53:40 +1100

    [NanoPB-Pico] Clean up acknowledgement and fix motor

--- src/nanopb.c
+++ src/nanopb.c
@@ -1,6 +1,6 @@
 #include "nanopb.h"
 
-
+static uint32_t Ack_Done = 0U;
 
 void nanopb_error(data_t *response){
     response->data[0] = 3U;
@@ -21,47 +21,41 @@ void write_stdio(data_t *buff){
 }
 
 
-/*
-Processing request and return message
-status: 0 for fail and 1 for sucess
-*/
-uint8_t process_request(data_t *request, data_t *response){
-    
+/**
+ * Processing Special 1 bytes request
+ */
+uint8_t special_request(data_t *request, data_t *response){
+
     Assignment msg = Assignment_init_zero;
-    uint8_t status = 0U;
     pb_ostream_t stream;
-    if(request->data[0] == 0U && request->data_len == 1U){
-        msg.asn = DevKind_Battery;
-        status = encode_request(response, &msg);
-        return status;
+    uint8_t status = 0U;
+    if(request->data[0] == 0U){
+        msg.asn = DevKind_MotorDriverL;
+        status = encode_request_msg(response, &msg);
+        Ack_Done  = 0U;
     }
-
-    status = decode_request(request, &msg);
-    if(status == 0U){
-        nanopb_error(response);
-    }
-
-    
-    if(status){
-        switch (msg.asn)
-        {
-        case DevKind_HostAck:
-            msg.asn = DevKind_DevAck;
-            status = encode_request(response, &msg);
-            break;
-    
-        default:
-            nanopb_error(response);
-            status = 0U;
-            break;
-        }
-    }
-
-
     return status;
 }
 
-uint8_t encode_request(data_t *buffer, Assignment *msg){
+uint8_t processing_msg_request(data_t *buffer, Assignment *msg){
+    uint8_t status;
+    switch (msg->asn)
+    {
+    case DevKind_HostAck:
+        msg->asn = DevKind_DevAck;
+        status = encode_request_msg(buffer, msg);
+        Ack_Done = 1U;
+        break;
+    
+    default:
+        nanopb_error(buffer);
+        status = 0U;
+        break;
+    }
+    return status;
+}
+
+uint8_t encode_request_msg(data_t *buffer, Assignment *msg){
     pb_ostream_t stream;
     stream = pb_ostream_from_buffer(buffer->data, PROTO_PACKET_SIZE);
     bool status = 0U;
@@ -76,7 +70,7 @@ uint8_t encode_request(data_t *buffer, Assignment *msg){
 }
 
 
-uint8_t decode_request(data_t *buff, Assignment *message){
+uint8_t decode_request_msg(data_t *buff, Assignment *message){
 
     /* Function to decoding message from ProtoBuf*/
     bool status;
@@ -87,4 +81,38 @@ uint8_t decode_request(data_t *buff, Assignment *message){
 
     return 1U;
     
+}
+
+/*
+Processing request and return message
+status: 0 for fail and 1 for sucess
+*/
+uint8_t process_request(data_t *request, data_t *response){
+    
+    Assignment msg = Assignment_init_zero;
+    uint8_t status = 0U;
+    pb_ostream_t stream;
+
+    if(request->data_len == 1U){
+        status = special_request(request, response);
+        return status;
+    }
+
+    if(Ack_Done == 0U){
+        status = decode_request_msg(request, &msg);
+        if(status == 1U){
+            status = processing_msg_request(response, &msg);
+            return status;
+        }
+    } else {
+
+    }
+
+    if(status == 0U){
+        nanopb_error(response);
+    }
+
+
+
+    return status;
 }
--- src/nanopb.h
+++ src/nanopb.h
@@ -29,9 +29,10 @@ typedef struct
 
 
 void write_stdio(data_t *buff);
-uint8_t decode_request(data_t *buff, Assignment *message);
 uint8_t process_request(data_t *request, data_t *response);
-uint8_t encode_request(data_t *buffer, Assignment *msg);
-
+uint8_t processing_msg_request(data_t *buffer, Assignment *msg);
+uint8_t encode_request_msg(data_t *buffer, Assignment *msg);
+uint8_t decode_request_msg(data_t *buff, Assignment *message);
+uint8_t special_request(data_t *request, data_t *response);
 
 #endif
